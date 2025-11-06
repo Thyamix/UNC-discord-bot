72ac4ff41d5164b86bb2e578485e0ffdb22078e1
Thyamix <85443364+Thyamix@users.noreply.github.com>
2025-11-06 11:05:38 +0100

    feat: changed task system

--- src/main/java/com/thyamix/tasks/StockpileStatusTask.java
+++ src/main/java/com/thyamix/tasks/StockpileStatusTask.java
@@ -1,20 +1,77 @@
 package com.thyamix.tasks;
 
 import com.thyamix.config.BotConfig;
+import com.thyamix.enums.StoredType;
 import com.thyamix.handlers.CommandHandler;
 import com.thyamix.utils.CSVStorage;
 import com.thyamix.utils.TaskRunner;
 
-public class StockpileStatusTask {
+import java.util.Optional;
+
+public class StockpileStatusTask implements Task {
+    private final long hour;
+
     private final CSVStorage storage;
     private final TaskRunner taskRunner;
+    private final CommandHandler commandHandler;
 
     public StockpileStatusTask(BotConfig config, CommandHandler commandHandler) {
         this.storage = new CSVStorage("RefreshChecker");
+        this.commandHandler = commandHandler;
+        this.taskRunner = new TaskRunner(config, this, commandHandler, storage);
 
-        this.taskRunner = new TaskRunner(config, commandHandler, storage);
+        this.hour = config.getSecondsInHour();
 
         taskRunner.start();
+    }
+
+    public void run() {
+        Optional<String[]> entryOptional = storage.getLastEntry();
+        String[] entry;
+        if (entryOptional.isPresent()) {
+            entry = entryOptional.get();
+        } else {
+            return;
+        }
+
+        long now = System.currentTimeMillis() / 1000;
+
+        long secondsAgo = now - Long.parseLong(entry[3]);
+
+        switch (StoredType.valueOf(entry[0])) {
+            case StoredType.REFRESH -> handleRefresh(secondsAgo);
+            case StoredType.FIRSTPING -> handleFirstPing(secondsAgo);
+            case StoredType.SECONDPING -> handleSecondPing(secondsAgo);
+            case StoredType.LASTPING -> handleLastPing(secondsAgo);
+        }
+    }
+
+    private void handleRefresh(long secondsAgo) {
+        if (secondsAgo >= hour * 36) {
+            this.commandHandler.alert("12 hours");
+            storage.addEntry(StoredType.FIRSTPING, "", "Bot", System.currentTimeMillis() / 1000);
+        }
+    }
+
+    private void handleFirstPing(long secondsAgo) {
+        if (secondsAgo >= hour * 8) {
+            this.commandHandler.alert("4 hours");
+            storage.addEntry(StoredType.SECONDPING, "", "Bot", System.currentTimeMillis() / 1000);
+        }
+    }
+
+    private void handleSecondPing(long secondsAgo) {
+        if (secondsAgo >= hour * 3) {
+            this.commandHandler.alert("1 hour");
+            storage.addEntry(StoredType.LASTPING, "", "Bot", System.currentTimeMillis() / 1000);
+        }
+    }
+
+    private void handleLastPing(long secondsAgo) {
+        if (secondsAgo >= hour) {
+            this.commandHandler.failed();
+            storage.addEntry(StoredType.EXPIRED, "", "Bot", System.currentTimeMillis() / 1000);
+        }
     }
 
     public CSVStorage getStorage() {
--- src/main/java/com/thyamix/utils/TaskRunner.java
+++ src/main/java/com/thyamix/utils/TaskRunner.java
@@ -3,6 +3,7 @@ package com.thyamix.utils;
 import com.thyamix.config.BotConfig;
 import com.thyamix.enums.StoredType;
 import com.thyamix.handlers.CommandHandler;
+import com.thyamix.tasks.Task;
 
 import java.util.Optional;
 import java.util.concurrent.Executors;
@@ -10,69 +11,22 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 public class TaskRunner {
-    private final long hour;
-
     private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
     private final CSVStorage storage;
     private final CommandHandler commandHandler;
 
     private final BotConfig config;
+    private final Task task;
 
-    public TaskRunner(BotConfig config, CommandHandler commandHandler, CSVStorage storage) {
+    public TaskRunner(BotConfig config, Task task, CommandHandler commandHandler, CSVStorage storage) {
         this.storage = storage;
         this.commandHandler = commandHandler;
         this.config = config;
-
-        this.hour = config.getSecondsInHour();
+        this.task = task;
     }
 
     public void start() {
-        scheduler.scheduleAtFixedRate(() -> {
-            Optional<String[]> entryOptional = storage.getLastEntry();
-            String[] entry;
-            if (entryOptional.isPresent()) {
-                entry = entryOptional.get();
-            } else {
-                return;
-            }
-
-            long now = System.currentTimeMillis() / 1000;
-
-            long secondsAgo = now - Long.parseLong(entry[3]);
-
-            switch (StoredType.valueOf(entry[0])) {
-                case StoredType.REFRESH -> handleRefresh(secondsAgo);
-                case StoredType.FIRSTPING -> handleFirstPing(secondsAgo);
-                case StoredType.SECONDPING -> handleSecondPing(secondsAgo);
-                case StoredType.LASTPING -> handleLastPing(secondsAgo);
-            }
-
-    }, 0, this.config.getPollingRate(), TimeUnit.SECONDS);
-    }
-
-    private void handleRefresh(long secondsAgo) {
-        if (secondsAgo >= hour * 36) {
-            commandHandler.alert("12 hours");
-            storage.addEntry(StoredType.FIRSTPING, "", "Bot", System.currentTimeMillis() / 1000);
-        }
-    }
-    private void handleFirstPing(long secondsAgo) {
-        if (secondsAgo >= hour * 8) {
-            commandHandler.alert("4 hours");
-            storage.addEntry(StoredType.SECONDPING, "", "Bot", System.currentTimeMillis() / 1000);
-        }
-    }
-    private void handleSecondPing(long secondsAgo) {
-        if (secondsAgo >= hour * 3) {
-            commandHandler.alert("1 hour");
-            storage.addEntry(StoredType.LASTPING, "", "Bot", System.currentTimeMillis() / 1000);
-        }
-    }
-    private void handleLastPing(long secondsAgo) {
-        if (secondsAgo >= hour) {
-            commandHandler.failed();
-            storage.addEntry(StoredType.EXPIRED, "", "Bot", System.currentTimeMillis() / 1000);
-        }
+        scheduler.scheduleAtFixedRate(this.task::run, 0, this.config.getPollingRate(), TimeUnit.SECONDS);
     }
 
     public void stop() {
